4b38b28c7e588648db94313f4de66ceaa0cf48e9
Lucas <lucasvalenteds@gmail.com>
2020-12-14 22:48:02 -0300

    Implementing endpoint to find all customers

---
+++ src/main/java/com/example/spring/customer/CustomerController.java
@@ -0,0 +1,31 @@
+package com.example.spring.customer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private static final Logger logger = LogManager.getLogger(CustomerController.class);
+
+    private static final List<Customer> customers = List.of(
+        new Customer("abc/123", "John Smith", 45),
+        new Customer("def/123", "Mary Jane", 32),
+        new Customer("def/456", "Paul Anderson", 18)
+    );
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Flux<Customer> findCustomers() {
+        logger.info("Returning all customers");
+
+        return Flux.fromIterable(customers);
+    }
+}
---
+++ src/test/java/com/example/spring/customer/CustomerControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.spring.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class CustomerControllerTest {
+
+    private final WebTestClient client = WebTestClient.bindToController(new CustomerController()).build();
+
+    @Test
+    void testFindingAllCustomers() {
+        List<Customer> customers = client.get()
+            .uri("/customers")
+            .exchange()
+            .expectStatus().isOk()
+            .expectBodyList(Customer.class)
+            .returnResult()
+            .getResponseBody();
+
+        assertNotNull(customers);
+        assertEquals(3, customers.size());
+        assertEquals("John Smith", customers.get(0).getName());
+        assertEquals("Mary Jane", customers.get(1).getName());
+        assertEquals("Paul Anderson", customers.get(2).getName());
+    }
+}
