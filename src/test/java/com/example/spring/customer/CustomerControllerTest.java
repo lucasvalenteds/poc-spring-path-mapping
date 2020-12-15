7e73df312158598875c4f2f9b2420ec5064ca1e2
Lucas <lucasvalenteds@gmail.com>
2020-12-14 23:11:47 -0300

    Wrapping unit tests in nested test suite

--- src/test/java/com/example/spring/customer/CustomerControllerTest.java
+++ src/test/java/com/example/spring/customer/CustomerControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.spring.customer;
 
 import com.example.spring.car.Car;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -13,76 +14,79 @@ class CustomerControllerTest {
 
     private final WebTestClient client = WebTestClient.bindToController(new CustomerController()).build();
 
-    @Test
-    void testFindingAllCustomers() {
-        List<Customer> customers = client.get()
-            .uri("/customers")
-            .exchange()
-            .expectStatus().isOk()
-            .expectBodyList(Customer.class)
-            .returnResult()
-            .getResponseBody();
+    @Nested
+    final class Encoded {
+        @Test
+        void testFindingAllCustomers() {
+            List<Customer> customers = client.get()
+                .uri("/customers")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Customer.class)
+                .returnResult()
+                .getResponseBody();
 
-        assertNotNull(customers);
-        assertEquals(3, customers.size());
-        assertEquals("John Smith", customers.get(0).getName());
-        assertEquals("Mary Jane", customers.get(1).getName());
-        assertEquals("Paul Anderson", customers.get(2).getName());
-    }
+            assertNotNull(customers);
+            assertEquals(3, customers.size());
+            assertEquals("John Smith", customers.get(0).getName());
+            assertEquals("Mary Jane", customers.get(1).getName());
+            assertEquals("Paul Anderson", customers.get(2).getName());
+        }
 
-    @Test
-    void testFindingOneCustomer() {
-        String customerId = "def/456";
+        @Test
+        void testFindingOneCustomer() {
+            String customerId = "def/456";
 
-        Customer customer = client.get()
-            .uri("/customers/{customerId}", customerId)
-            .exchange()
-            .expectStatus().isOk()
-            .expectBody(Customer.class)
-            .returnResult()
-            .getResponseBody();
+            Customer customer = client.get()
+                .uri("/customers/{customerId}", customerId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Customer.class)
+                .returnResult()
+                .getResponseBody();
 
-        assertNotNull(customer);
-        assertEquals("def/456", customer.getId());
-        assertEquals("Paul Anderson", customer.getName());
-    }
+            assertNotNull(customer);
+            assertEquals("def/456", customer.getId());
+            assertEquals("Paul Anderson", customer.getName());
+        }
 
-    @Test
-    void testFindingCarsFromCustomer() {
-        String customerId = "def/123";
+        @Test
+        void testFindingCarsFromCustomer() {
+            String customerId = "def/123";
 
-        List<Car> cars = client.get()
-            .uri("/customers/{customerId}/cars", customerId)
-            .exchange()
-            .expectStatus().isOk()
-            .expectBodyList(Car.class)
-            .returnResult()
-            .getResponseBody();
+            List<Car> cars = client.get()
+                .uri("/customers/{customerId}/cars", customerId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Car.class)
+                .returnResult()
+                .getResponseBody();
 
-        assertNotNull(cars);
-        assertEquals(2, cars.size());
-        assertEquals("xyz/100", cars.get(0).getId());
-        assertEquals("Ford", cars.get(0).getBrand());
-        assertEquals("xyz/101", cars.get(1).getId());
-        assertEquals("Toyota", cars.get(1).getBrand());
-    }
+            assertNotNull(cars);
+            assertEquals(2, cars.size());
+            assertEquals("xyz/100", cars.get(0).getId());
+            assertEquals("Ford", cars.get(0).getBrand());
+            assertEquals("xyz/101", cars.get(1).getId());
+            assertEquals("Toyota", cars.get(1).getBrand());
+        }
 
-    @Test
-    void testFindingCarFromCustomer() {
-        String customerId = "abc/123";
-        String carId = "xyz/102";
+        @Test
+        void testFindingCarFromCustomer() {
+            String customerId = "abc/123";
+            String carId = "xyz/102";
 
-        Car car = client.get()
-            .uri("/customers/{customerId}/cars/{carId}", customerId, carId)
-            .exchange()
-            .expectStatus().isOk()
-            .expectBody(Car.class)
-            .returnResult()
-            .getResponseBody();
+            Car car = client.get()
+                .uri("/customers/{customerId}/cars/{carId}", customerId, carId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Car.class)
+                .returnResult()
+                .getResponseBody();
 
-        assertNotNull(car);
-        assertEquals(carId, car.getId());
-        assertEquals("Toyota", car.getBrand());
-        assertEquals("compact", car.getCategory());
+            assertNotNull(car);
+            assertEquals(carId, car.getId());
+            assertEquals("Toyota", car.getBrand());
+            assertEquals("compact", car.getCategory());
+        }
     }
 }
