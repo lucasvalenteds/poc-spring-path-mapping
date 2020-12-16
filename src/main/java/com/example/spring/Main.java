c95aeb059377e239e99dc4f6156ea17c7f8d6b02
Lucas <lucasvalenteds@gmail.com>
2020-12-15 23:50:32 -0300

    Merging Main and AppConfiguration

--- src/main/java/com/example/spring/Main.java
+++ src/main/java/com/example/spring/Main.java
@@ -3,19 +3,25 @@ package com.example.spring;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
 import reactor.netty.http.server.HttpServer;
 
 import java.time.Duration;
 
+@EnableWebFlux
+@ComponentScan("com.example.spring")
+@PropertySource("classpath:application.properties")
 public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
 
         Environment environment = context.getEnvironment();
 
