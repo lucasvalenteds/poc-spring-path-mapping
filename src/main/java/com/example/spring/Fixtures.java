5dd3a1bcfa40bea3ecf662d3021f9dfd1a362835
Lucas <lucasvalenteds@gmail.com>
2020-12-15 22:25:37 -0300

    Extracting fixtures to Fixtures class

---
+++ src/main/java/com/example/spring/Fixtures.java
@@ -0,0 +1,32 @@
+package com.example.spring;
+
+import com.example.spring.car.Car;
+import com.example.spring.customer.Customer;
+
+import java.util.List;
+import java.util.Map;
+
+public final class Fixtures {
+
+    private Fixtures() {
+    }
+
+    public static final List<Customer> customers = List.of(
+        new Customer("abc/123", "John Smith", 45),
+        new Customer("def/123", "Mary Jane", 32),
+        new Customer("def/456", "Paul Anderson", 18)
+    );
+
+    public static final List<Car> cars = List.of(
+        new Car("xyz/100", "Ford", "truck"),
+        new Car("xyz/101", "Toyota", "truck"),
+        new Car("xyz/102", "Toyota", "compact")
+    );
+
+    public static final Map<String, List<Car>> customersCars = Map.ofEntries(
+        Map.entry(customers.get(0).getId(), List.of(cars.get(2))),
+        Map.entry(customers.get(1).getId(), List.of(cars.get(0), cars.get(1))),
+        Map.entry(customers.get(2).getId(), List.of())
+    );
+
+}
--- src/main/java/com/example/spring/customer/CustomerController.java
+++ src/main/java/com/example/spring/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.spring.customer;
 
+import com.example.spring.Fixtures;
 import com.example.spring.car.Car;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,7 +13,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/customers")
@@ -20,37 +20,18 @@ public class CustomerController {
 
     private static final Logger logger = LogManager.getLogger(CustomerController.class);
 
-    private static final List<Customer> customers = List.of(
-        new Customer("abc/123", "John Smith", 45),
-        new Customer("def/123", "Mary Jane", 32),
-        new Customer("def/456", "Paul Anderson", 18)
-    );
-
-    private static final List<Car> cars = List.of(
-        new Car("xyz/100", "Ford", "truck"),
-        new Car("xyz/101", "Toyota", "truck"),
-        new Car("xyz/102", "Toyota", "compact")
-    );
-
-    private static final Map<String, List<Car>> customersCars = Map.ofEntries(
-        Map.entry(customers.get(0).getId(), List.of(cars.get(2))),
-        Map.entry(customers.get(1).getId(), List.of(cars.get(0), cars.get(1))),
-        Map.entry(customers.get(2).getId(), List.of())
-    );
-
-
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Customer> findCustomers() {
         logger.info("Returning all customers");
 
-        return Flux.fromIterable(customers);
+        return Flux.fromIterable(Fixtures.customers);
     }
 
     @GetMapping(path = "/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Customer> findCustomer(@PathVariable("customerId") String customerId) {
         logger.info("Returning customer with ID {}", customerId);
 
-        return Flux.fromIterable(customers)
+        return Flux.fromIterable(Fixtures.customers)
             .filter(customer -> customer.getId().equals(customerId))
             .singleOrEmpty();
     }
@@ -59,7 +40,7 @@ public class CustomerController {
     public Flux<Car> findCustomerCars(@PathVariable("customerId") String customerId) {
         logger.info("Returning cars from customer with ID {}", customerId);
 
-        return Flux.fromIterable(customersCars.getOrDefault(customerId, List.of()));
+        return Flux.fromIterable(Fixtures.customersCars.getOrDefault(customerId, List.of()));
     }
 
     @GetMapping(path = "/{customerId}/cars/{carId}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -68,7 +49,7 @@ public class CustomerController {
     ) {
         logger.info("Returning car with ID {} from customer with ID {}", carId, customerId);
 
-        return Flux.fromIterable(customersCars.getOrDefault(customerId, List.of()))
+        return Flux.fromIterable(Fixtures.customersCars.getOrDefault(customerId, List.of()))
             .filter(car -> car.getId().equals(carId))
             .singleOrEmpty();
     }
